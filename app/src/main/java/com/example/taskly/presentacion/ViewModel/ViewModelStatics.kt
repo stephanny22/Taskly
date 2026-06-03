d69871541c5ef80debcd8ff8559fe76902ac66df
Lorenaaaaa13 <ilrubiom@udistrital.edu.co>
2026-06-03 11:50:19 -0500

    arreglar la pantalla de estadisticas

--- app/src/main/java/com/example/taskly/presentacion/ViewModel/ViewModelStatics.kt
+++ app/src/main/java/com/example/taskly/presentacion/ViewModel/ViewModelStatics.kt
@@ -15,16 +15,32 @@ data class StatisticsUiState(
 
 class ViewModelStatics(
     private val noteRepo: NoteRepository,
+    private val userId: String
 ) : ViewModel() {
 
-    val uiState: StateFlow<StatisticsUiState> = noteRepo.notes.map { notes ->
-        val total     = notes.size
-        val completed = notes.count { it.status == "completed" }
-        StatisticsUiState(
-            total     = total,
-            completed = completed,
-            pending   = total - completed,
-            progress  = if (total > 0) completed.toFloat() / total else 0f,
-        )
-    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), StatisticsUiState())
-}
+    val uiState: StateFlow<StatisticsUiState> =
+        noteRepo.notes.map { notes ->
+
+            val userNotes = notes.filter {
+                it.id_user == userId
+            }
+
+            val total = userNotes.size
+            val completed = userNotes.count {
+                it.status == "completed"
+            }
+
+            StatisticsUiState(
+                total = total,
+                completed = completed,
+                pending = total - completed,
+                progress = if (total > 0)
+                    completed.toFloat() / total
+                else
+                    0f
+            )
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            StatisticsUiState()
+        )}
